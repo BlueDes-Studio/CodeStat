f2d61a8393c80e3775f663614dac713468acba2b
saisumith770 <saisumith812@gmail.com>
2024-02-11 18:39:57 +0530

    chore!: setting up tailwindcss with storybook

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,11 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/stories/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    colors: {
+      bgPrimary: "#0B0B0B",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
