60a808941f502ee1e357a8c9bd3a7e508295320c
saisumith770 <saisumith812@gmail.com>
2024-02-12 02:32:18 +0530

    fix: modified the AppWrapper to include Branding

--- src/stories/AppWrapper.tsx
+++ src/stories/AppWrapper.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren } from "react";
+import { Branding } from "./Branding";
 
 export interface AppWrapperProps {
   /**
@@ -22,13 +23,18 @@ export const AppWrapper: React.FC<Props> = ({
   visibile = true,
   children,
 }) => {
-  if (!visibile) return <div className="h-screen w-screen"></div>;
+  if (!visibile) return <div className="h-screen w-screen">{children}</div>;
   return (
     <>
       <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center overflow-hidden bg-bgPrimary">
         {overlayLogo && <img src="/bg-overlay-logo.svg" />}
       </div>
-      <div className="absolute left-0 top-0 h-screen w-screen">{children}</div>
+      <div className="absolute left-0 top-0 h-screen w-screen">
+        <div className="px-5 pt-4">
+          <Branding scale={75} />
+          <div className="ml-4">{children}</div>
+        </div>
+      </div>
     </>
   );
 };
