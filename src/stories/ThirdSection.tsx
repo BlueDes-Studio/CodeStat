c35d4998ee00f7df0a281e5586314f28e43fcf19
Gautam Mishra <njoygautam12@gmail.com>
2024-03-05 22:51:14 +0530

    Add new charting libraries and dependencies

--- src/stories/ThirdSection.tsx
+++ src/stories/ThirdSection.tsx
@@ -3,7 +3,98 @@ import React from "react";
 
 const ThirdSection: React.FC = () => {
     return(
-        <div></div>
+        <div className="flex gap-[42px]">
+            <div className="w-[545px] h-[402px] relative bg-opacity-40 bg-slate-900 rounded-[20px] hover:opacity-100">
+                <div className="w-10 h-10 left-[432px] top-[336px] absolute opacity-20 hover:opacity-100">
+                    <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl"></div>
+                </div>
+                <div className="h-[138px] left-[90px] top-[132px] absolute flex-col justify-center items-center gap-[19px] inline-flex">
+                    <div className="text-white text-[32px] font-semibold font-[Inter] mr-[88px]">Unified Heatmap </div>
+                    <div className="self-stretch opacity-30 text-white text-base font-normal font-['SF Compact Text'] leading-tight hover:opacity-100">Sync coding progress from LeetCode, Coding Ninja, and GeeksforGeeks. Your cumulative calendar marks every solved question, offering a visual journey of your coding success.</div>
+                </div>
+                <div className="w-10 h-10 left-[-6px] top-[270px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                    <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[25px] top-[346px] absolute opacity-20 hover:opacity-100">
+                    <div className="w-10 h-10 left-0 top-0 absolute bg-violet-700 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[9px] top-[178px] absolute opacity-20 rounded-[18.67px] hover:opacity-100">
+                    <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl shadow-inner border-8 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[-3px] top-[-17px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                    <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[-6px] top-[47px] absolute opacity-20 hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl border-4 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[29px] top-[11px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[164px] top-0 absolute opacity-20 hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-700 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[204px] top-[-9px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[448px] top-[-21px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[286px] top-0 absolute opacity-20 rounded-[18.67px] hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl shadow-inner border-8 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[334px] top-[-9px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[504px] top-[28px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[499px] top-[96px] absolute opacity-20 rounded-[18.67px] hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl shadow-inner border-8 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[483px] top-[246px] absolute opacity-20 hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl border-4 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[505px] top-[346px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[459px] top-[362px] absolute opacity-20 rounded-[18.67px] hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl shadow-inner border-8 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[341px] top-[375px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[253px] top-[355px] absolute opacity-20 rounded-[18.67px] hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl shadow-inner border-8 border-fuchsia-300"></div>
+                </div>
+                <div className="w-10 h-10 left-[176px] top-[366px] absolute opacity-20 hover:opacity-100">
+                <div className="w-10 h-10 left-0 top-0 absolute bg-violet-500 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[120px] top-[375px] absolute opacity-20 justify-start items-start gap-2.5 inline-flex">
+                <div className="w-10 h-10 bg-indigo-950 rounded-xl"></div>
+                </div>
+                <div className="w-10 h-10 left-[102px] top-[375px] absolute opacity-20"></div>
+                <div className="w-10 h-10 left-[395px] top-[7px] absolute opacity-20 hover:opacity-100">
+                    <div className="w-10 h-10 left-0 top-0 absolute bg-violet-700 rounded-xl"></div>
+
+                </div>
+            </div>
+
+
+     <div className="w-[485px] h-[300px] relative bg-opacity-50 bg-slate-900 rounded-[20px]">
+        
+    <div className="w-[60px] h-32 left-[37px] top-[236px] absolute opacity-60 bg-zinc-800 rounded-[10px] delay-100 translate-y-3"></div>
+    <div className="w-[60px] h-[199px] left-[114px] top-[199px] absolute opacity-60 bg-zinc-800 rounded-[10px] delay-100 translate-y-6"></div>
+    <div className="w-[60px] h-[199px] left-[191px] top-[273px] absolute opacity-60 bg-zinc-800 rounded-[10px] delay-100 translate-y-5"></div>
+    <div className="w-[60px] h-[199px] left-[268px] top-[236px] absolute opacity-60 bg-zinc-800 rounded-[10px] delay-100 translate-y-7"></div>
+    <div className="w-[60px] h-[199px] left-[345px] top-[193px] absolute opacity-60 bg-zinc-800 rounded-[10px] delay-100 translate-y-3"></div>
+
+
+
+            <div className="w-96 text-white text-[32px] font-semibold font-['Inter'] mx-[51px] mt-[57px]">Upcoming events and<br/>your coding legacy stats</div>
+            <div className="w-[378px] opacity-30 text-white text-base font-normal font-['SF Compact Text'] leading-tight mx-[51px] mt-[28px] hover:opacity-75">Stay ahead with Contest Alerts! Get updates on upcoming coding contests across platforms and track your progress with detailed stats from past contests.</div>
+
+            </div>
+        </div>
     );
 };
 
